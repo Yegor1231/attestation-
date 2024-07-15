2491b3d592f6f5ffcccc00fdad8ebaf48df6ecd9
Yegor1231 <159125602+Yegor1231@users.noreply.github.com>
2024-07-15 22:54:08 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,27 +1,18 @@
 ﻿void Main()
 {
-    int m = 3;
-    int n = 5;
+    int m = 4;
 
-    Console.WriteLine($"A({m}, {n}) = {AkkerFunc(m, n)}");
+    int n = 12;
+
+    Console.WriteLine($"Натуральные числа от {m} до {n}:");
+    Console.WriteLine(PrintNaturalNumbers(m, n));
+
 }
 
-int AkkerFunc(int m, int n)
+string PrintNaturalNumbers(int m, int n)
 {
-    if (m == 0)
-    {
-        return n + 1;
-    }
-    else if (m > 0 && n == 0)
-    {
-        return AkkerFunc(m - 1, 1);
-    }
-    else if (m > 0 && n > 0)
-    {
-        return AkkerFunc(m - 1, AkkerFunc(m, n - 1));
-    }
-    return(AkkerFunc(m,n));
-
+    if (m == n) return Convert.ToString(n);
+    return m + ", " + PrintNaturalNumbers(m + 1, n);
 }
 
 Main();
