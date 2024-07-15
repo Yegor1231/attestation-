c1f7938429fad0cd01f664c64635aa570ded0c30
Yegor <gubenko_ev@mail.ru>
2024-07-15 22:39:20 +0500

    Homework

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿void Main()
+{
+    int m = 4;
+
+    int n = 12;
+
+    Console.WriteLine($"Натуральные числа от {m} до {n}:");
+    Console.WriteLine(PrintNaturalNumbers(m, n));
+
+}
+
+string PrintNaturalNumbers(int m, int n)
+{
+    if (m == n) return Convert.ToString(n);
+    return m + ", " + PrintNaturalNumbers(m + 1, n);
+}
+
+Main();
